29625536bf1d84e4fdbebf418e7c8b9b03c7b7f3
Max tier4ik Shevchenko <tier4ik@gmail.com>
2019-01-23 14:03:55 +0400

    add custom scroll to projects.html

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,9 +1,13 @@
 const Velocity = require('velocity-animate/velocity.min.js');
 var {curtainGo} = require('./modules/curtain.js');
-
+const GeminiScrollbar = require('gemini-scrollbar');
 require('./../sass/projects.sass');
 
 $(window).on('load', ()=> {
     $('body').css('opacity', 1);
     curtainGo();
+
+    var myScrollbar = new GeminiScrollbar({
+        element: document.querySelector('.html__box')
+    }).create();
 });
